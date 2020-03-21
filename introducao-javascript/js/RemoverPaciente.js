8e132eea02f12d85f7926a20bbe9e2fd48e69085
Felipe Duque <felipe@duque.dev>
2020-03-21 15:04:33 -0300

    remover adicionado

---
+++ introducao-javascript/js/RemoverPaciente.js
@@ -0,0 +1,7 @@
+var pacientes = document.querySelectorAll(".paciente");
+
+pacientes.forEach(function(paciente) {
+    paciente.addEventListener("dblclick", function(){
+    this.remove();
+    });
+});
