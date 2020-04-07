5c364e2a9fcbd6046f88842514dd8f68dc66d9eb
Felipe Duque <felipe@duque.dev>
2020-04-07 10:42:43 -0300

    Criando minha primeira linha de NoSQL

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,13 +1,27 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
 //Iniciando o app
 const app = express();
 
 //Iniciando o DB
-mongoose.connect(`mongodb://localhost:27017/nodejs`, { useNewUrlParser: true , useUnifiedTopology:true })
+mongoose.connect(`mongodb://localhost:27017/nodejs`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+requireDir("./src/models"); //Importa todos os Models!
+
+const Product = mongoose.model("Product");
 
 //Primeira rota
-app.get('/', (req, res) => {res.send(`Hello, World! I'm Felipe Duque`)})
+app.get("/", (req, res) => {
+  Product.create({
+    title: "React Native",
+    description: "Build native apps with React",
+    url: "http://github.com/facebook/react-native",
+  });
+  return res.send(`Hello, World! I'm Felipe Duque`);
+});
 
 app.listen(3001);
